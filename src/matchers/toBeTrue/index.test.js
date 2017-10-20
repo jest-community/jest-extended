892c90567f2006fecb1839244b7c767b09ad56cb
Matt Phillips <matt@mattphillips.io>
2017-10-20 20:35:02 +0100

    Add tests for when toBeTrue fails

--- src/matchers/toBeTrue/index.test.js
+++ src/matchers/toBeTrue/index.test.js
@@ -2,17 +2,27 @@ import each from 'jest-each';
 
 import matcher from './';
 
-describe('toBeTrue', () => {
-  expect.extend(matcher);
+expect.extend(matcher);
 
+describe('toBeTrue', () => {
   it('passes when given true', () => {
     expect(true).toBeTrue();
   });
 
+  it('fails when not given true', () => {
+    expect(() => expect(false).toBeTrue()).toThrowErrorMatchingSnapshot();
+  });
+});
+
+describe('.not.toBeTrue', () => {
   each([[false], [''], [0], [{}], [[]], [() => {}], [undefined], [null], [NaN]]).it(
     'passes when not given true: %s',
     given => {
       expect(given).not.toBeTrue();
     }
   );
+
+  it('fails when given true', () => {
+    expect(() => expect(true).not.toBeTrue()).toThrowErrorMatchingSnapshot();
+  });
 });
