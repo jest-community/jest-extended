686ce1f0234764422d2570482159530eb3110123
Matt Phillips <matt@mattphillips.io>
2017-10-20 20:14:04 +0100

    Add red text highlighting to package error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { red } from 'chalk';
+
 import matchers from './matchers';
 
 const jestExpect = global.expect;
@@ -5,9 +7,13 @@ const jestExpect = global.expect;
 if (jestExpect !== undefined) {
   jestExpect.extend(matchers);
 } else {
+  /* eslint-disable no-console */
   console.error(
-    "Unable to find Jest's global expect." +
-      '\nPlease check you have added jest-extended correctly to your jest configuration.' +
-      '\nSee https://github.com/mattphillips/jest-extended#setup for help.'
+    red(
+      "Unable to find Jest's global expect." +
+        '\nPlease check you have added jest-extended correctly to your jest configuration.' +
+        '\nSee https://github.com/mattphillips/jest-extended#setup for help.'
+    )
   );
+  /* eslint-enable no-console */
 }
