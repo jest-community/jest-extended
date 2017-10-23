acce0d76d2fede91dfced2a5c7884c418f27f81d
Brandon Newton <btnwtn@gmail.com>
2017-10-23 14:54:50 -0400

    Add pass/fail messaging

--- src/matchers/toEqualCaseInsensitive/index.js
+++ src/matchers/toEqualCaseInsensitive/index.js
@@ -1,9 +1,42 @@
+import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
+import diff from 'jest-diff';
 import predicate from './predicate';
 
+const passMessage = (received, expected) => () => {
+  return (
+    matcherHint('.not.toEqualCaseInsensitive') +
+    '\n\n' +
+    'Expected values to not be equal (using ===):\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(received)}`
+  );
+};
+
+const failMessage = (received, expected) => () => {
+  const diffString = diff(expected, received, {
+    expand: this.expand
+  });
+
+  return (
+    matcherHint('.toEqualCaseInsensitive') +
+    '\n\n' +
+    'Expected values to be equal (using ===):\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(received)}` +
+    (diffString ? `\n\nDifference:\n\n${diffString}` : '')
+  );
+};
+
 export default {
-  toEqualCaseInsensitive: (received, expected) => {
+  toEqualCaseInsensitive(received, expected) {
     const pass = predicate(received, expected);
 
-    return { pass, message: 'lol', actual: received };
+    return {
+      pass,
+      message: pass ? passMessage(received, expected) : failMessage(received, expected),
+      actual: received
+    };
   }
 };
