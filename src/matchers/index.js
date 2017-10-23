1bcc9bf2e5e2b6a1f45f95173059c605d888d3b9
Massimo Prencipe <massimo.prencipe@solita.fi>
2017-10-23 22:17:21 +0300

    Issue #34, toBeObject matcher

--- src/matchers/index.js
+++ src/matchers/index.js
@@ -1,4 +1,5 @@
 import toBeTrue from './toBeTrue';
 import toContainValue from './toContainValue';
+import toBeObject from './toBeObject';
 
-export default [toBeTrue, toContainValue].reduce((acc, matcher) => ({ ...acc, ...matcher }), {});
+export default [toBeTrue, toContainValue, toBeObject].reduce((acc, matcher) => ({ ...acc, ...matcher }), {});
