77dfb135023e4fc6defb5575a3d1bf2b4191823d
Amish Shah <amishshah.2k@gmail.com>
2017-10-23 23:57:05 +0100

    Fix edge case

--- src/matchers/toBePositive/predicate.js
+++ src/matchers/toBePositive/predicate.js
@@ -1,1 +1,1 @@
-export default expected => !isNaN(expected) && expected !== Infinity && expected > 0;
+export default expected => expected !== true && !isNaN(expected) && expected !== Infinity && expected > 0;
