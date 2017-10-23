2a859ff8e4ef7afc1f184874bc3bf05a98289564
Matt Phillips <matt@mattphillips.io>
2017-10-23 16:00:22 +0100

    Add basic object.values polyfill

--- src/matchers/toContainValue/predicate.js
+++ src/matchers/toContainValue/predicate.js
@@ -1,3 +1,6 @@
 import { equals } from 'expect/build/jasmine_utils';
 
-export default (actual, value) => !!Object.values(actual).find(v => equals(v, value));
+export default (actual, value) =>
+  !!Object.keys(actual)
+    .map(k => actual[k])
+    .find(v => equals(v, value));
