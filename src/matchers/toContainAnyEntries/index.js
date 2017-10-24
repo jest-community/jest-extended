d0d012ff13591f9a9ae11ea3e12ae52361c8b0c3
vikneshwar <lviknesh@gmail.com>
2017-10-25 01:26:10 +0530

    Fix test case failure

--- src/matchers/toContainAnyEntries/index.js
+++ src/matchers/toContainAnyEntries/index.js
@@ -6,17 +6,17 @@ const passMessage = (object, entries) => () =>
   matcherHint('.not.toContainAnyEntries') +
   '\n\n' +
   'Expected object should not contain any of the provided entries:\n' +
-  `  ${printExpected(object)}\n` +
+  `  ${printExpected(entries)}\n` +
   'Received:\n' +
-  `  ${printReceived(entries)}`;
+  `  ${printReceived(object)}`;
 
 const failMessage = (object, entries) => () =>
   matcherHint('.toContainAnyEntries') +
   '\n\n' +
   'Expected object should contain atleast one of the provided entries:\n' +
-  `  ${printExpected(object)}\n` +
+  `  ${printExpected(entries)}\n` +
   'Received:\n' +
-  `  ${printReceived(entries)}`;
+  `  ${printReceived(object)}`;
 
 export default {
   toContainAnyEntries: (object, entries) => {
