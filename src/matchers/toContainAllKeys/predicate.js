71a4b30255c2b747d4a06cb088a77872f41274eb
Fredrik Mäkilä <fredrik.makila@gmail.com>
2017-10-24 23:27:50 +0100

    Clean up

--- src/matchers/toContainAllKeys/predicate.js
+++ src/matchers/toContainAllKeys/predicate.js
@@ -2,8 +2,5 @@ import { contains } from '../../utils';
 
 export default (object, keys) => {
   const objectKeys = Object.keys(object);
-
-  console.log(objectKeys.every(key => keys.indexOf(key) >= 0));
-
   return objectKeys.every(objKey => contains(keys, objKey));
 };
