273a8e7ae589f7bf14feedc4b38418eeb7c6f9bc
Matt Phillips <matt@mattphillips.io>
2017-10-24 19:40:20 +0100

    Rename prefix to suffix

--- src/matchers/toEndWith/index.js
+++ src/matchers/toEndWith/index.js
@@ -2,29 +2,29 @@ import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
 
 import predicate from './predicate';
 
-const passMessage = (prefix, string) => () =>
+const passMessage = (suffix, string) => () =>
   matcherHint('.not.toEndWith') +
   '\n\n' +
   'Expected string to not end with:\n' +
-  `  ${printExpected(prefix)}\n` +
+  `  ${printExpected(suffix)}\n` +
   'Received:\n' +
   `  ${printReceived(string)}`;
 
-const failMessage = (prefix, string) => () =>
+const failMessage = (suffix, string) => () =>
   matcherHint('.toEndWith') +
   '\n\n' +
   'Expected string to end with:\n' +
-  `  ${printExpected(prefix)}\n` +
+  `  ${printExpected(suffix)}\n` +
   'Received:\n' +
   `  ${printReceived(string)}`;
 
 export default {
-  toEndWith: (string, prefix) => {
-    const pass = predicate(prefix, string);
+  toEndWith: (string, suffix) => {
+    const pass = predicate(suffix, string);
     if (pass) {
-      return { pass: true, message: passMessage(prefix, string) };
+      return { pass: true, message: passMessage(suffix, string) };
     }
 
-    return { pass: false, message: failMessage(prefix, string) };
+    return { pass: false, message: failMessage(suffix, string) };
   }
 };
