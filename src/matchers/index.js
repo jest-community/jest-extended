47358ce417523baf31bd010b2b4839b2716b0b7a
Gary Leutheuser <GaryLeutheuser@users.noreply.github.com>
2017-10-24 04:36:48 -0400

    Add to start with matcher (#53)
    * Add to start with matcher
    
    * Add toStartWith to index.js export
    
    * Improve to start with error message/matcher

--- src/matchers/index.js
+++ src/matchers/index.js
@@ -1,9 +1,9 @@
-import toBeTrue from './toBeTrue';
 import toContainValue from './toContainValue';
 import toContainValues from './toContainValues';
 import toEqualCaseInsensitive from './toEqualCaseInsensitive';
+import toStartWith from './toStartWith';
 
-export default [toBeTrue, toContainValue, toContainValues, toEqualCaseInsensitive].reduce(
+export default [toBeTrue, toContainValue, toContainValues, toEqualCaseInsensitive, toStartWith].reduce(
   (acc, matcher) => ({ ...acc, ...matcher }),
   {}
 );
---
+++ src/matchers/toStartWith/index.js
@@ -0,0 +1,30 @@
+import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
+
+import predicate from './predicate';
+
+const passMessage = (prefix, string) => () =>
+  matcherHint('.not.toStartWith', 'received', '') +
+  '\n\n' +
+  'Expected string to not start with:\n' +
+  `  ${printReceived(prefix)}\n` +
+  'Received:\n' +
+  `  ${printReceived(prefix)}`;
+
+const failMessage = (prefix, string) => () =>
+  matcherHint('.toStartWith', 'received', '') +
+  '\n\n' +
+  'Expected string to start with:\n' +
+  `  ${printExpected(prefix)}\n` +
+  'String started with:\n' +
+  `  ${printReceived(string.substring(0, prefix.length))}`;
+
+export default {
+  toStartWith: (string, prefix) => {
+    const pass = predicate(prefix, string);
+    if (pass) {
+      return { pass: true, message: passMessage(prefix, string) };
+    }
+
+    return { pass: false, message: failMessage(prefix, string) };
+  }
+};
