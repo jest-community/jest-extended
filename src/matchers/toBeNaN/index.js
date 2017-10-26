642924ac2614f82e12d10ef165e1e12bc3d92135
Matt Phillips <matt@mattphillips.io>
2017-10-26 11:51:07 +0100

    Reformat toBeNan error messages

--- src/matchers/toBeNaN/index.js
+++ src/matchers/toBeNaN/index.js
@@ -1,19 +1,17 @@
-import { matcherHint, printExpected, printReceived } from 'jest-matcher-utils';
+import { matcherHint, printReceived } from 'jest-matcher-utils';
 
 import predicate from './predicate';
 
 const passMessage = received => () =>
   matcherHint('.not.toBeNaN', 'received', '') +
   '\n\n' +
-  'Expected value to be a number:\n' +
+  'Expected value to be a number received:\n' +
   `  ${printReceived(received)}`;
 
 const failMessage = received => () =>
   matcherHint('.toBeNaN', 'received', '') +
   '\n\n' +
-  'Expected value to not be a number:\n' +
-  `  ${printExpected(true)}\n` +
-  'Received:\n' +
+  'Expected value to not be a number received:\n' +
   `  ${printReceived(received)}`;
 
 export default {
