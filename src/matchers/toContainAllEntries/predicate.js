f0512e5165f06e105260a7a2ba60314b79a8c3a1
Matt Phillips <matt@mattphillips.io>
2017-10-27 17:33:37 +0100

    Add deep equality checks to toContainAllEnties;

--- src/matchers/toContainAllEntries/predicate.js
+++ src/matchers/toContainAllEntries/predicate.js
@@ -1,7 +1,9 @@
+import { equals } from 'expect/build/jasmine_utils';
+
 export default (obj, entries) => {
   if (!obj.hasOwnProperty || entries.length != Object.keys(obj).length) {
     return false;
   }
 
-  return !entries.some(([key, value]) => !obj.hasOwnProperty(key) || obj[key] !== value);
+  return entries.every(([key, value]) => obj.hasOwnProperty(key) && equals(obj[key], value));
 };
