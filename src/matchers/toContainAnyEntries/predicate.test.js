d88064196404c3d934fb61320185df53bce866ec
vikneshwar <lviknesh@gmail.com>
2017-10-28 01:32:17 +0530

    Fix test coverage

--- src/matchers/toContainAnyEntries/predicate.test.js
+++ src/matchers/toContainAnyEntries/predicate.test.js
@@ -4,10 +4,10 @@ const data = { a: 'foo', b: 'bar', c: 'baz' };
 
 describe('.toContainAnyEntries', () => {
   it('passes when given object contains key', () => {
-    expect(predicate(data, [['a', 'qux'], ['a', 'foo']])).toBe(true);
+    expect(predicate(data, [['a', 'qux'], ['a', 'foo'], ['x', 'foo']])).toBe(true);
   });
 
   it('fails when given object does not contain key', () => {
-    expect(predicate(data, [['a', 'qux'], ['b', 'foo']])).toBe(false);
+    expect(predicate(data, [['a', 'qux'], ['b', 'foo'], ['x', 'foo']])).toBe(false);
   });
 });
