234ea17959497848138109e6656fd06980c0368d
Subinoy Ghosh <subinoy7@users.noreply.github.com>
2017-10-29 14:55:50 +0530

    Update index.test.js

--- src/matchers/toBeNil/index.test.js
+++ src/matchers/toBeNil/index.test.js
@@ -12,6 +12,9 @@ describe('.toBeNil', () => {
   it('passes when undefined is given', () => {
     expect(undefined).toBeNil();
   });
+  it('fails when not undefined is given', () => {
+    expect(() => expect('value').toBeNil()).toThrowErrorMatchingSnapshot();
+  });
 });
 
 describe('.not.toBeNil', () => {
@@ -20,5 +23,9 @@ describe('.not.toBeNil', () => {
     [{}]
   ]).it('passes when item is not null or undefined : %s', given => {
     expect(given).not.toBeNil();
+  });
+
+  it('pass when undefined is given', () => {
+    expect(() => expect(null).not.toBeNil()).toThrowErrorMatchingSnapshot();
   });
 });
