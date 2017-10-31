4a5060b4f17f23c55eb4c141a7229eac8801d5b5
Subinoy Ghosh <subinoy7@users.noreply.github.com>
2017-11-01 01:22:19 +0530

    new matcher toBeNil (#96)
    * toBeNil added
    
    * Update index.test.js
    
    * Update index.test.js
    
    * Add files via upload
    
    * added new test file for index.js
    
    * entry of tBeNil
    
    * Add files via upload
    
    * updated the test files for better readability
    
    Updated the code as suggested
    
    * updating contribution list
    
    * fixed eslint warning
    
    fixed eslint "warning  Newline required at end of file but not found  eol-last"
    
    * Update index.test.js
    
    * updated snapshot after changin in index.test.js
    
    * updated Readme.md
    
    merged with latest Readme.md

--- src/matchers/index.js
+++ src/matchers/index.js
@@ -35,6 +35,7 @@ import toContainAllEntries from './toContainAllEntries';
 import toContainEntries from './toContainEntries';
 import toContainAnyEntries from './toContainAnyEntries';
 import toContainAnyValues from './toContainAnyValues';
+import toBeNil from './toBeNil';
 import toSatisfyAll from './toSatisfyAll';
 
 export default [
@@ -75,5 +76,6 @@ export default [
   toContainEntries,
   toContainAnyEntries,
   toContainAnyValues,
+  toBeNil,
   toSatisfyAll
 ].reduce((acc, matcher) => ({ ...acc, ...matcher }), {});
