3fda960988e1f528514e50bd64c250a6af3cc300
Gilles De Mey <gilles.de.mey@gmail.com>
2018-07-30 16:51:24 +0200

    adds toBeBefore and toBeAfter matchers (#117) (#137)
    * adds toBeBefore and toBeAfter matchers (#117)
    
    * updates documentation
    
    * adds gillesdemey to contributors
    
    * encode timezone into date constructors

---
+++ src/matchers/toBeAfter/predicate.js
@@ -0,0 +1,5 @@
+function toBeAfter(date, after) {
+  return date > after;
+}
+
+export default (date, after) => toBeAfter(date, after);
---
+++ src/matchers/toBeBefore/index.test.js
@@ -0,0 +1,30 @@
+import matcher from './';
+
+expect.extend(matcher);
+
+const EARLIER = new Date('2018-06-01T22:00:00.000Z');
+const LATER = new Date('2018-06-02T22:00:00.000Z');
+
+describe('.toBeBefore', () => {
+  test('passes when given an earlier date', () => {
+    expect(EARLIER).toBeBefore(LATER);
+  });
+
+  test('fails when given a later date', () => {
+    expect(() => {
+      expect(LATER).toBeBefore(EARLIER);
+    }).toThrowErrorMatchingSnapshot();
+  });
+});
+
+describe('.not.toBeBefore', () => {
+  test('passes when given an earlier date', () => {
+    expect(LATER).not.toBeBefore(EARLIER);
+  });
+
+  test('fails when given an earlier date', () => {
+    expect(() => {
+      expect(EARLIER).not.toBeBefore(LATER);
+    }).toThrowErrorMatchingSnapshot();
+  });
+});
