230d07120a902362e64e27d5d2da28fb2956feb7
Matt Phillips <matt@mattphillips.io>
2018-07-30 15:01:17 +0100

    Add better type of check to toBeObject matcher

--- src/matchers/toBeObject/index.test.js
+++ src/matchers/toBeObject/index.test.js
@@ -9,15 +9,21 @@ describe('.toBeObject', () => {
     expect({}).toBeObject();
   });
 
-  each([[false], [''], [0], [() => {}], [undefined], [NaN]]).test('fails when not given an object: %s', given => {
-    expect(() => expect(given).toBeObject()).toThrowErrorMatchingSnapshot();
-  });
+  each([[false], [''], [0], [() => {}], [undefined], [NaN], [[1, 2, 3]]]).test(
+    'fails when not given an object: %s',
+    given => {
+      expect(() => expect(given).toBeObject()).toThrowErrorMatchingSnapshot();
+    }
+  );
 });
 
 describe('.not.toBeObject', () => {
-  each([[false], [''], [0], [() => {}], [undefined], [NaN]]).test('passes when not given an object: %s', given => {
-    expect(given).not.toBeObject();
-  });
+  each([[false], [''], [0], [() => {}], [undefined], [NaN], [[1, 2, 3]]]).test(
+    'passes when not given an object: %s',
+    given => {
+      expect(given).not.toBeObject();
+    }
+  );
 
   test('fails when given an object', () => {
     expect(() => expect({}).not.toBeObject()).toThrowErrorMatchingSnapshot();
