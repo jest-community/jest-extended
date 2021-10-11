a2904bd6b9d1ec215fcf006b00788ea90cf561bf
Keegan Witt <keeganwitt@gmail.com>
2021-10-11 04:42:52 -0400

    Don't invoke expect.extend (#333)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
-import * as matchers from './matchers';
-
-const jestExpect = global.expect;
-
-if (jestExpect !== undefined) {
-  jestExpect.extend(matchers);
-} else {
-  /* eslint-disable no-console */
-  console.error(
-    "Unable to find Jest's global expect." +
-      '\nPlease check you have added jest-extended correctly to your jest configuration.' +
-      '\nSee https://github.com/jest-community/jest-extended#setup for help.',
-  );
-  /* eslint-enable no-console */
-}
+export * from './matchers';
