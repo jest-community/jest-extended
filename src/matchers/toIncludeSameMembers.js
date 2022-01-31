8d62f8db1bb4ad149593d3a32a06f486e8fe7e55
Matt Phillips <matt@mattphillips.io>
2022-01-31 15:50:44 +0000

    Condense `toIncludeSameMembers` matcher into one file

---
+++ src/matchers/toIncludeSameMembers.js
@@ -0,0 +1,43 @@
+export function toIncludeSameMembers(actual, expected) {
+  const { printReceived, printExpected, matcherHint } = this.utils;
+
+  const passMessage =
+    matcherHint('.not.toIncludeSameMembers') +
+    '\n\n' +
+    'Expected list to not exactly match the members of:\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const failMessage =
+    matcherHint('.toIncludeSameMembers') +
+    '\n\n' +
+    'Expected list to have the following members and no more:\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const pass = predicate(this.equals, actual, expected);
+
+  return { pass, message: () => (pass ? passMessage : failMessage) };
+}
+
+const predicate = (equals, actual, expected) => {
+  if (!Array.isArray(actual) || !Array.isArray(expected) || actual.length !== expected.length) {
+    return false;
+  }
+
+  const remaining = expected.reduce((remaining, secondValue) => {
+    if (remaining === null) return remaining;
+
+    const index = remaining.findIndex(firstValue => equals(secondValue, firstValue));
+
+    if (index === -1) {
+      return null;
+    }
+
+    return remaining.slice(0, index).concat(remaining.slice(index + 1));
+  }, actual);
+
+  return !!remaining && remaining.length === 0;
+};
