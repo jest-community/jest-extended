d2c7a99817b7ceef4712f4bc4260b4b10eeecd75
Matt Phillips <matt@mattphillips.io>
2022-01-31 15:11:55 +0000

    Condense `toContainValues` matcher into one file

--- src/matchers/toContainValues/predicate.js
+++
@@ -1,6 +0,0 @@
-import { contains } from '../../utils';
-
-export default (equals, object, values) => {
-  const objectValues = Object.keys(object).map(k => object[k]);
-  return values.every(value => contains(equals, objectValues, value));
-};
