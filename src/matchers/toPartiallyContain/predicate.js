74939194d7dd6b1656b477b5e9375b871cdfe1ac
Matt Phillips <matt@mattphillips.io>
2022-01-31 15:52:52 +0000

    Condense `toPartiallyContain` matcher into one file

--- src/matchers/toPartiallyContain/predicate.js
+++
@@ -1,8 +0,0 @@
-import { containsEntry } from '../../utils';
-
-export default (equals, array, item) =>
-  Array.isArray(array) &&
-  Array.isArray([item]) &&
-  [item].every(partial =>
-    array.some(value => Object.entries(partial).every(entry => containsEntry(equals, value, entry))),
-  );
