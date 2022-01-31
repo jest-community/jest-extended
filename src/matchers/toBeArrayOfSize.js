b6e429cff41bd2451419862b783da21aed81c341
Matt Phillips <matt@mattphillips.io>
2022-01-31 10:42:08 +0000

    Fix broken snapshots

--- src/matchers/toBeArrayOfSize.js
+++ src/matchers/toBeArrayOfSize.js
@@ -5,21 +5,21 @@ export function toBeArrayOfSize(actual, expected) {
 
   const passMessage = () =>
     `${matcherHint('.not.toBeArrayOfSize')}
-  
-  Expected value to not be an array of size:
-    ${printExpected(expected)}
-  Received:
-    value: ${printReceived(actual)}
-    length: ${printReceived(determinePropertyMessage(actual, 'length'))}`;
+
+Expected value to not be an array of size:
+  ${printExpected(expected)}
+Received:
+  value: ${printReceived(actual)}
+  length: ${printReceived(determinePropertyMessage(actual, 'length'))}`;
 
   const failMessage = () =>
     `${matcherHint('.toBeArrayOfSize')}
-  
-  Expected value to be an array of size:
-    ${printExpected(expected)}
-  Received:
-    value: ${printReceived(actual)}
-    length: ${printReceived(determinePropertyMessage(actual, 'length'))}`;
+
+Expected value to be an array of size:
+  ${printExpected(expected)}
+Received:
+  value: ${printReceived(actual)}
+  length: ${printReceived(determinePropertyMessage(actual, 'length'))}`;
 
   const pass = Array.isArray(actual) && actual.length === expected;
   const message = pass ? passMessage : failMessage;
--- src/matchers/toBeBefore.js
+++ src/matchers/toBeBefore.js
@@ -3,14 +3,14 @@ export function toBeBefore(actual, expected) {
   const passMessage = () =>
     matcherHint('.not.toBeBefore', 'received', '') +
     '\n\n' +
-    `Expected date to be before ${printReceived(actual)} but received:\n` +
-    `  ${printReceived(expected)}`;
+    `Expected date to be before ${printReceived(expected)} but received:\n` +
+    `  ${printReceived(actual)}`;
 
   const failMessage = () =>
     matcherHint('.toBeBefore', 'received', '') +
     '\n\n' +
-    `Expected date to be before ${printReceived(actual)} but received:\n` +
-    `  ${printReceived(expected)}`;
+    `Expected date to be before ${printReceived(expected)} but received:\n` +
+    `  ${printReceived(actual)}`;
 
   const pass = actual < expected;
   const message = pass ? passMessage : failMessage;
