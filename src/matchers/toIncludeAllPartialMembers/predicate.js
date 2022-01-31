c978845ece88fb57996e490a3fdff5b8c6ada52b
Matt Phillips <matt@mattphillips.io>
2022-01-31 15:42:55 +0000

    Condense `toIncludeAllPartialMembers` matcher into one file

--- src/matchers/toIncludeAllPartialMembers/predicate.js
+++
@@ -1,8 +0,0 @@
-import { containsEntry } from '../../utils';
-
-export default (equals, array, set) =>
-  Array.isArray(array) &&
-  Array.isArray(set) &&
-  set.every(partial =>
-    array.some(value => Object.entries(partial).every(entry => containsEntry(equals, value, entry))),
-  );
--- src/matchers/toPartiallyContain/predicate.js
+++ src/matchers/toPartiallyContain/predicate.js
@@ -1,3 +1,8 @@
-import toIncludeAllPartialMembers from '../toIncludeAllPartialMembers/predicate';
+import { containsEntry } from '../../utils';
 
-export default (equals, array, item) => toIncludeAllPartialMembers(equals, array, [item]);
+export default (equals, array, item) =>
+  Array.isArray(array) &&
+  Array.isArray([item]) &&
+  [item].every(partial =>
+    array.some(value => Object.entries(partial).every(entry => containsEntry(equals, value, entry))),
+  );
