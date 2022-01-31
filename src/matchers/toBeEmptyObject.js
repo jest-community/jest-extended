6b69371ba34d338d97224302b88398a14ed65769
Matt Phillips <matt@mattphillips.io>
2022-01-31 11:03:13 +0000

    Condense `toBeEmptyObject` matcher into one file

---
+++ src/matchers/toBeEmptyObject.js
@@ -0,0 +1,22 @@
+import { getType } from 'jest-get-type';
+
+export function toBeEmptyObject(actual) {
+  const { printReceived, matcherHint } = this.utils;
+
+  const passMessage = () =>
+    matcherHint('.not.toBeEmptyObject', 'received', '') +
+    '\n\n' +
+    'Expected value to not be an empty object, received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const failMessage = () =>
+    matcherHint('.toBeEmptyObject', 'received', '') +
+    '\n\n' +
+    'Expected value to be an empty object, received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const pass = getType(actual) === 'object' && Object.keys(actual).length === 0;
+  const message = pass ? passMessage : failMessage;
+
+  return { pass, message };
+}
