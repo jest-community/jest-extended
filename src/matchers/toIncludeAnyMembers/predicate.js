2feb7f60cd44e8b59909211e29a80c727e0dbf42
Matt Phillips <matt@mattphillips.io>
2022-01-31 15:44:31 +0000

    Condense `toIncludeAnyMembers` matcher into one file

--- src/matchers/toIncludeAnyMembers/predicate.js
+++
@@ -1,5 +0,0 @@
-import { contains } from '../../utils';
-
-export default (equals, array, members) => {
-  return Array.isArray(array) && Array.isArray(members) && members.some(member => contains(equals, array, member));
-};
