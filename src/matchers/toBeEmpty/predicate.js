b948a9d400a29c20dc6fb75e4a45b3c434f97af0
Matt Phillips <matt@mattphillips.io>
2022-01-31 11:00:55 +0000

    Condense `toBeEmpty` matcher into one file

--- src/matchers/toBeEmpty/predicate.js
+++
@@ -1,9 +0,0 @@
-const isEmptyIterable = value => {
-  if (typeof value[Symbol.iterator] !== 'function') {
-    return false;
-  }
-  const firstIteration = value[Symbol.iterator]().next();
-  return firstIteration.done;
-};
-
-export default (equals, value) => equals({}, value) || isEmptyIterable(value);
