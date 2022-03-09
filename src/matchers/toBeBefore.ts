1704a52a61bf5d781fbc2b90ef4de05836505925
Igor Wessel <igor_wessel@hotmail.com>
2022-03-09 07:24:22 -0300

    feat: convert toBeBefore matcher to typescript

--- src/matchers/toBeBefore.ts
+++ src/matchers/toBeBefore.ts
@@ -1,4 +1,18 @@
-export function toBeBefore(actual, expected) {
+interface CustomMatchers<R = unknown> {
+  toBeBefore(date: Date): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toBeBefore(this: jest.MatcherContext, actual: unknown, expected: Date): jest.CustomMatcherResult {
   const { matcherHint, printReceived } = this.utils;
   const passMessage =
     matcherHint('.not.toBeBefore', 'received', '') +
