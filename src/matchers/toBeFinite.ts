5e355d1cad9fc17a91849bef659106785c8d781b
Igor Wessel <igor_wessel@hotmail.com>
2022-03-15 04:17:05 -0300

    feat: convert toBeFinite to typescript

--- src/matchers/toBeFinite.ts
+++ src/matchers/toBeFinite.ts
@@ -1,4 +1,18 @@
-export function toBeFinite(actual) {
+interface CustomMatchers<R = unknown> {
+  toBeFinite(): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toBeFinite(this: jest.MatcherContext, actual: unknown): jest.CustomMatcherResult {
   const { printReceived, matcherHint } = this.utils;
 
   const passMessage =
