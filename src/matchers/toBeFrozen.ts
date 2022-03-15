c485be8259bf54986584702ffc8bf8185e807bd8
Igor Wessel <igor_wessel@hotmail.com>
2022-03-15 04:17:16 -0300

    feat: convert toBeFrozen to typescript

--- src/matchers/toBeFrozen.ts
+++ src/matchers/toBeFrozen.ts
@@ -1,4 +1,18 @@
-export function toBeFrozen(actual) {
+interface CustomMatchers<R = unknown> {
+  toBeFrozen(): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toBeFrozen(this: jest.MatcherContext, actual: unknown): jest.CustomMatcherResult {
   const { matcherHint } = this.utils;
 
   const passMessage = matcherHint('.not.toBeFrozen', 'received', '') + '\n\n' + 'Expected object to not be frozen';
