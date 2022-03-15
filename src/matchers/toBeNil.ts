31511897c2ac182bea057813cc7d7896f1107082
Igor Wessel <igor_wessel@hotmail.com>
2022-03-15 04:20:14 -0300

    feat: convert toBeNill to typescript

--- src/matchers/toBeNil.ts
+++ src/matchers/toBeNil.ts
@@ -1,4 +1,18 @@
-export function toBeNil(actual) {
+interface CustomMatchers<R = unknown> {
+  toBeNil(): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toBeNil(this: jest.MatcherContext, actual: unknown): jest.CustomMatcherResult {
   const { printReceived, matcherHint } = this.utils;
 
   const passMessage =
