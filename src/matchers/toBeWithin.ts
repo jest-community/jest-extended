902188eab1128de3c77f9314f33b53f48747f14c
Igor Wessel <igor_wessel@hotmail.com>
2022-03-15 04:26:15 -0300

    feat: convert toBeWithin to typescript

--- src/matchers/toBeWithin.ts
+++ src/matchers/toBeWithin.ts
@@ -1,4 +1,23 @@
-export function toBeWithin(actual, start, end) {
+interface CustomMatchers<R = unknown> {
+  toBeWithin(start: number, end: number): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toBeWithin(
+  this: jest.MatcherContext,
+  actual: unknown,
+  start: number,
+  end: number,
+): jest.CustomMatcherResult {
   const { printReceived, printExpected, matcherHint } = this.utils;
 
   const passMessage =
@@ -17,7 +36,7 @@ export function toBeWithin(actual, start, end) {
     'Received:\n' +
     `  ${printReceived(actual)}`;
 
-  const pass = actual >= start && actual < end;
+  const pass = (actual as number) >= start && (actual as number) < end;
 
   return { pass, message: () => (pass ? passMessage : failMessage) };
 }
