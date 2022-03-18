7c84276ea443c4848a9927f37dbb23c3ed2014b2
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 03:35:29 -0300

    feat: convert toContainAnyEntries to typescript

---
+++ src/matchers/toContainAnyEntries.ts
@@ -0,0 +1,55 @@
+import { contains } from '../utils';
+
+interface CustomMatchers<R = unknown, T = Record<string, unknown>> {
+  toContainAnyEntries(entries: Array<[keyof T, T[keyof T]]>): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R, T> extends CustomMatchers<R, T> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toContainAnyEntries<T = unknown>(
+  this: jest.MatcherContext,
+  actual: T,
+  expected: Array<[keyof T, T[keyof T]]>,
+): jest.CustomMatcherResult {
+  const matcherName = 'toContainAnyEntries';
+  const { RECEIVED_COLOR, printReceived, printExpected, printWithType, matcherHint, matcherErrorMessage } = this.utils;
+
+  if (typeof actual !== 'object' || actual === null) {
+    throw new Error(
+      matcherErrorMessage(
+        matcherHint(matcherName),
+        `${RECEIVED_COLOR('received')} value must be a non-null object`,
+        printWithType('Received', actual, printReceived),
+      ),
+    );
+  }
+
+  const passMessage =
+    matcherHint(`.not.${matcherName}`) +
+    '\n\n' +
+    'Expected object to not contain any of the provided entries:\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const failMessage =
+    matcherHint(`.${matcherName}`) +
+    '\n\n' +
+    'Expected object to contain any of the provided entries:\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const entries = Object.entries(actual);
+  const pass = expected.some(entry => contains(this.equals, entries, entry));
+
+  return { pass, message: () => (pass ? passMessage : failMessage) };
+}
