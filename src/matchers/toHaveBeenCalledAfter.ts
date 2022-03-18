68981f52d00a2a6aec5966d999c279aa72da4a55
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 03:38:04 -0300

    feat: convert toHaveBeenCalledAfter to typescript

--- src/matchers/toHaveBeenCalledAfter.ts
+++ src/matchers/toHaveBeenCalledAfter.ts
@@ -1,6 +1,24 @@
 import { isJestMockOrSpy } from '../utils';
 
-export function toHaveBeenCalledAfter(actual, expected) {
+interface CustomMatchers<R = unknown> {
+  toHaveBeenCalledAfter(mock: jest.Mock): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toHaveBeenCalledAfter(
+  this: jest.MatcherContext,
+  actual: jest.Mock,
+  expected: jest.Mock,
+): jest.CustomMatcherResult {
   const { printReceived, printExpected, matcherHint } = this.utils;
 
   if (!isJestMockOrSpy(actual)) {
@@ -34,9 +52,9 @@ export function toHaveBeenCalledAfter(actual, expected) {
   return { pass, message: () => (pass ? passMessage : failMessage) };
 }
 
-const smallest = ns => ns.reduce((acc, n) => (acc < n ? acc : n));
+const smallest = (ns: number[]) => ns.reduce((acc, n) => (acc < n ? acc : n));
 
-const predicate = (firstInvocationCallOrder, secondInvocationCallOrder) => {
+const predicate = (firstInvocationCallOrder: number[], secondInvocationCallOrder: number[]) => {
   if (firstInvocationCallOrder.length === 0) return true;
   if (secondInvocationCallOrder.length === 0) return false;
 
@@ -46,15 +64,16 @@ const predicate = (firstInvocationCallOrder, secondInvocationCallOrder) => {
   return firstSmallest > secondSmallest;
 };
 
-const mockCheckFailMessage = (utils, value, isReceivedValue) => () => {
-  const valueKind = isReceivedValue ? 'Received' : 'Expected';
-  const valueKindPrintFunc = isReceivedValue ? utils.printReceived : utils.printExpected;
+const mockCheckFailMessage =
+  (utils: jest.MatcherContext['utils'], value: jest.Mock, isReceivedValue: boolean) => () => {
+    const valueKind = isReceivedValue ? 'Received' : 'Expected';
+    const valueKindPrintFunc = isReceivedValue ? utils.printReceived : utils.printExpected;
 
-  return (
-    utils.matcherHint('.toHaveBeenCalledAfter') +
-    '\n\n' +
-    `Matcher error: ${valueKindPrintFunc(valueKind.toLowerCase())} must be a mock or spy function` +
-    '\n\n' +
-    utils.printWithType(valueKind, value, valueKindPrintFunc)
-  );
-};
+    return (
+      utils.matcherHint('.toHaveBeenCalledAfter') +
+      '\n\n' +
+      `Matcher error: ${valueKindPrintFunc(valueKind.toLowerCase())} must be a mock or spy function` +
+      '\n\n' +
+      utils.printWithType(valueKind, value, valueKindPrintFunc)
+    );
+  };
