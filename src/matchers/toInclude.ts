22474eb6627000de2e3e6bbbeea220b0cde26ef6
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 03:38:31 -0300

    feat: convert toInclude to typescript

--- src/matchers/toInclude.ts
+++ src/matchers/toInclude.ts
@@ -1,4 +1,18 @@
-export function toInclude(actual, expected) {
+interface CustomMatchers<R = unknown> {
+  toInclude(substring: string): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toInclude(this: jest.MatcherContext, actual: string, expected: string): jest.CustomMatcherResult {
   const { printReceived, printExpected, matcherHint } = this.utils;
 
   const passMessage =
