0263f6c2622fc65da99607792af6107bdea4da23
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 03:38:48 -0300

    feat: convert toIncludeRepeated to typescript

--- src/matchers/toIncludeRepeated.ts
+++ src/matchers/toIncludeRepeated.ts
@@ -1,4 +1,23 @@
-export function toIncludeRepeated(actual, expected, occurrences) {
+interface CustomMatchers<R = unknown> {
+  toIncludeRepeated(substring: string, times: number): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toIncludeRepeated(
+  this: jest.MatcherContext,
+  actual: string,
+  expected: string,
+  occurrences: number,
+): jest.CustomMatcherResult {
   const { printReceived, printExpected, matcherHint } = this.utils;
 
   const passMessage =
