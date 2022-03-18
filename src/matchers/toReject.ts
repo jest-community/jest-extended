77972549528ee0bfe45c6343c3a30ccea06db010
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 03:38:57 -0300

    feat: convert toReject to typescript

--- src/matchers/toReject.ts
+++ src/matchers/toReject.ts
@@ -1,4 +1,18 @@
-export async function toReject(actual) {
+interface CustomMatchers<R = unknown> {
+  toReject(): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export async function toReject(this: jest.MatcherContext, actual: Promise<unknown>): Promise<jest.CustomMatcherResult> {
   const { matcherHint } = this.utils;
 
   const passMessage =
