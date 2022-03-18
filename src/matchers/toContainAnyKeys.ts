fc1c2f122b162db3ee4f6f665bd387f87d1b5431
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 03:35:41 -0300

    feat: convert toContainAnyKeys to typescript

---
+++ src/matchers/toContainAnyKeys.ts
@@ -0,0 +1,52 @@
+interface CustomMatchers<R = unknown, T = Record<string, unknown>> {
+  toContainAnyKeys(keys: Array<keyof T>): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R, T> extends CustomMatchers<R, T> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toContainAnyKeys<T = unknown>(
+  this: jest.MatcherContext,
+  actual: T,
+  expected: Array<keyof T>,
+): jest.CustomMatcherResult {
+  const matcherName = 'toContainAnyKeys';
+  const { RECEIVED_COLOR, printReceived, printExpected, printWithType, matcherHint, matcherErrorMessage } = this.utils;
+
+  if (typeof actual !== 'object' || actual === null) {
+    throw new Error(
+      matcherErrorMessage(
+        matcherHint(matcherName),
+        `${RECEIVED_COLOR('received')} value must be a non-null object`,
+        printWithType('Received', actual, printReceived),
+      ),
+    );
+  }
+
+  const passMessage =
+    matcherHint(`.not.${matcherName}`) +
+    '\n\n' +
+    'Expected object not to contain any of the following keys:\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const failMessage =
+    matcherHint(`.${matcherName}`) +
+    '\n\n' +
+    'Expected object to contain any of the following keys:\n' +
+    `  ${printExpected(expected)}\n` +
+    'Received:\n' +
+    `  ${printReceived(actual)}`;
+
+  const pass = expected.some(key => Object.prototype.hasOwnProperty.call(actual, key));
+
+  return { pass, message: () => (pass ? passMessage : failMessage) };
+}
