08a998cca19c0e11cae723d65798ed5e78818a63
Igor Wessel <igor_wessel@hotmail.com>
2022-03-18 03:42:33 -0300

    feat: convert utils/index to typescript

---
+++ src/utils/index.ts
@@ -0,0 +1,27 @@
+type Equals = <A, B>(a: A, b: B) => boolean;
+
+export const contains = <A, B>(equals: Equals, list: A[], value: B): boolean => {
+  return list.findIndex(item => equals(item, value)) > -1;
+};
+
+export const determinePropertyMessage = <A extends { [key: string]: unknown }>(
+  actual: A,
+  property: keyof A,
+  message = 'Not Accessible',
+) => {
+  return actual && Object.hasOwnProperty.call(actual, property) ? actual[property] : message;
+};
+
+export const isJestMockOrSpy = (value: jest.Mock) => {
+  return !!(
+    value &&
+    (value as unknown as { _isMockFunction: boolean })._isMockFunction === true &&
+    typeof value.mock === 'object'
+  );
+};
+
+export const containsEntry = <A extends Record<string, unknown>>(
+  equals: Equals,
+  obj: A,
+  [key, value]: [keyof A, A[keyof A]],
+): boolean => obj.hasOwnProperty && Object.prototype.hasOwnProperty.call(obj, key) && equals(obj[key], value);
