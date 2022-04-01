49fe22c07e1bb9705ff89e67ab435dff02f1fbeb
Igor Wessel <igor_wessel@hotmail.com>
2022-04-01 06:59:22 -0300

    feat: convert toSatisfyAny to typescript

--- src/matchers/toSatisfyAny.ts
+++ src/matchers/toSatisfyAny.ts
@@ -1,4 +1,22 @@
-export function toSatisfyAny(actual, expected) {
+interface CustomMatchers<R = unknown> {
+  toSatisfyAny<A>(matcher: (actual: A) => boolean): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toSatisfyAny<A>(
+  this: jest.MatcherContext,
+  actual: A[],
+  expected: <P>(matcher: P) => boolean,
+): jest.CustomMatcherResult {
   const { printReceived, printExpected, matcherHint } = this.utils;
 
   const passMessage =
