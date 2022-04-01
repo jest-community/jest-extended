f5997811ceba0b7243ebf0a637090f72d023176e
Igor Wessel <igor_wessel@hotmail.com>
2022-04-01 07:11:19 -0300

    feat: convert toIncludeAllPartialMembers to typescript

--- src/matchers/toIncludeAllPartialMembers.ts
+++ src/matchers/toIncludeAllPartialMembers.ts
@@ -1,6 +1,24 @@
 import { containsEntry } from '../utils';
 
-export function toIncludeAllPartialMembers(actual, expected) {
+interface CustomMatchers<R = unknown, T = Record<string, unknown>> {
+  toIncludeAllMembers(members: T[]): R;
+}
+
+declare global {
+  namespace jest {
+    interface Matchers<R> extends CustomMatchers<R> {}
+
+    interface Expect extends CustomMatchers {}
+
+    interface InverseAsymmetricMatchers extends CustomMatchers {}
+  }
+}
+
+export function toIncludeAllPartialMembers<E>(
+  this: jest.MatcherContext,
+  actual: unknown[],
+  expected: E[],
+): jest.CustomMatcherResult {
   const { printReceived, printExpected, matcherHint } = this.utils;
 
   const passMessage =
