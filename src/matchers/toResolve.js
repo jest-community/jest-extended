5c7a3b730b58b1a1cb3c20799fc6b1ffce66d368
Harel Moshe <harlemo@wix.com>
2022-08-08 13:47:26 +0300

    Refactor

--- src/matchers/toResolve.js
+++ src/matchers/toResolve.js
@@ -1,36 +1,31 @@
 export async function toResolve(actual) {
   const { matcherHint } = this.utils;
 
+  const expectedToResolveButGotRejectedMessage = (value, { isNot } = { isNot: false }) =>
+    matcherHint('toResolve', 'received', '', { isNot, promise: true }) +
+    '\n\n' +
+    'Expected promise to resolve, however it rejected with: "' +
+    value +
+    '".\n';
+
+  const expectedToRejectButGotResolvedMessage = (value, { isNot } = { isNot: false }) =>
+    matcherHint('toResolve', 'received', '', { isNot, promise: true }) +
+    '\n\n' +
+    'Expected promise to reject, however it resolved with: "' +
+    value +
+    '".\n';
+
   const matcherResult = await actual.then(
     value => ({
       pass: true,
       message: () =>
         this.isNot
-          ? matcherHint('toResolve', 'received', '', { isNot: true, promise: true }) +
-            '\n\n' +
-            'Expected promise to reject, however it resolved with: "' +
-            value +
-            '".\n'
-          : matcherHint('toResolve', 'received', '', { isNot: false, promise: true }) +
-            '\n\n' +
-            'Expected promise to resolve, however it rejected with: "' +
-            value +
-            '".\n',
+          ? expectedToRejectButGotResolvedMessage(value, { isNot: this.isNot })
+          : expectedToResolveButGotRejectedMessage(value, { isNot: this.isNot }),
     }),
     value => ({
       pass: false,
-      message: () =>
-        this.isNot
-          ? matcherHint('toResolve', 'received', '', { isNot: true, promise: true }) +
-            '\n\n' +
-            'Expected promise to resolve, however it rejected with: "' +
-            value +
-            '".\n'
-          : matcherHint('toResolve', 'received', '', { isNot: false, promise: true }) +
-            '\n\n' +
-            'Expected promise to resolve, however it rejected with: "' +
-            value +
-            '".\n',
+      message: () => expectedToResolveButGotRejectedMessage(value, { isNot: this.isNot }),
     }),
   );
 
