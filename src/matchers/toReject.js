14d718d5ca3df4154f8c080811203015c38aa0bb
Harel Moshe <harlemo@wix.com>
2022-08-11 18:49:04 +0300

    toReject

--- src/matchers/toReject.js
+++ src/matchers/toReject.js
@@ -1,16 +1,31 @@
 export async function toReject(actual) {
-  const { matcherHint } = this.utils;
+  const { matcherHint, printReceived } = this.utils;
 
-  const passMessage =
-    matcherHint('.not.toReject', 'received', '') + '\n\n' + 'Expected promise to resolve, however it rejected.\n';
+  const expectedToResolveButGotRejectedMessage = (value, { isNot } = { isNot: false }) =>
+    matcherHint('toReject', 'received', '', { isNot, promise: true }) +
+    '\n\n' +
+    'Expected promise to resolve, however it rejected with: "' +
+    printReceived(value) +
+    '".\n';
 
-  const failMessage =
-    matcherHint('.toReject', 'received', '') + '\n\n' + 'Expected promise to reject, however it resolved.\n';
+  const expectedToRejectButGotResolvedMessage = (value, { isNot } = { isNot: false }) =>
+    matcherHint('toReject', 'received', '', { isNot, promise: true }) +
+    '\n\n' +
+    'Expected promise to reject, however it resolved with: "' +
+    printReceived(value) +
+    '".\n';
 
-  const pass = await actual.then(
-    () => false,
-    () => true,
+  return actual.then(
+    value => ({
+      pass: false,
+      message: () => expectedToRejectButGotResolvedMessage(value, { isNot: this.isNot }),
+    }),
+    value => ({
+      pass: true,
+      message: () =>
+        this.isNot
+          ? expectedToResolveButGotRejectedMessage(value, { isNot: this.isNot })
+          : expectedToRejectButGotResolvedMessage(value, { isNot: this.isNot }),
+    }),
   );
-
-  return { pass, message: () => (pass ? passMessage : failMessage) };
 }
