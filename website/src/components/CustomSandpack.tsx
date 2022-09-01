fe21980cfec37034f25a7274c70e92d1b61a1d3d
Matt Phillips <matt@mattphillips.io>
2022-09-01 16:55:43 +0100

    Add interactive matchers with `Sandpack`

---
+++ website/src/components/CustomSandpack.tsx
@@ -0,0 +1,62 @@
+import {
+  Sandpack as SandpackDefault,
+  SandpackCodeEditor,
+  SandpackLayout,
+  SandpackLayout as SandpackLayoutDefault,
+  SandpackProvider,
+  SandpackTests,
+} from '@codesandbox/sandpack-react';
+import React from 'react';
+
+const setup = `import * as matchers from 'jest-extended';
+
+expect.extend(matchers);`;
+
+export const Sandpack: React.FC<{ files: any }> = props => {
+  return (
+    <SandpackProvider
+      customSetup={{ entry: 'entry.ts', dependencies: { 'jest-extended': '^3.0.2' } }}
+      files={{
+        '/entry.ts': {
+          code: '',
+          hidden: true,
+        },
+        '/extended.test.ts': {
+          code: setup,
+          hidden: true,
+        },
+        ...props.files,
+      }}
+    >
+      <SandpackLayout>
+        <SandpackCodeEditor showTabs />
+        <SandpackTests verbose />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
+};
+
+export const TestFile: React.FC<{ name: string; children: string }> = props => {
+  return (
+    <SandpackProvider
+      theme="dark"
+      customSetup={{ entry: 'entry.js', dependencies: { 'jest-extended': '^3.0.2' } }}
+      files={{
+        '/entry.js': {
+          code: '',
+          hidden: true,
+        },
+        '/extended.test.js': {
+          code: setup,
+          hidden: true,
+        },
+        [`/${props.name}.test.js`]: props.children,
+      }}
+    >
+      <SandpackLayout>
+        <SandpackCodeEditor showTabs />
+        <SandpackTests />
+      </SandpackLayout>
+    </SandpackProvider>
+  );
+};
