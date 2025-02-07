1d060e5a35264653f04d48d924c88dd800c104ea
Leslie Hoare <iam@lesleh.co.uk>
2025-02-07 12:51:58 +0000

    Update toBeHexadecimal.js to support alpha channel

--- src/matchers/toBeHexadecimal.js
+++ src/matchers/toBeHexadecimal.js
@@ -1,7 +1,11 @@
 export function toBeHexadecimal(actual) {
   const { printReceived, matcherHint } = this.utils;
 
-  const pass = longRegex.test(actual) || shortRegex.test(actual);
+  const pass =
+    longRegex.test(actual) ||
+    longRegexWithAlpha.test(actual) ||
+    shortRegex.test(actual) ||
+    shortRegexWithAlpha.test(actual);
 
   return {
     pass,
@@ -19,4 +23,6 @@ export function toBeHexadecimal(actual) {
 }
 
 const longRegex = RegExp(/^#\b[a-f0-9]{6}\b/gi);
+const longRegexWithAlpha = RegExp(/^#\b[a-f0-9]{6}\b/gi);
 const shortRegex = RegExp(/^#\b[a-f0-9]{3}\b/gi);
+const shortRegexWithAlpha = RegExp(/^#\b[a-f0-9]{6}\b/gi);
