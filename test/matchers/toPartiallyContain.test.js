7eb7627b2b1db014065ac9b10090c28459145e46
Keegan Witt <keeganwitt@gmail.com>
2025-04-14 23:14:43 -0400

    Fix lint issue (#741)

--- test/matchers/toPartiallyContain.test.js
+++ test/matchers/toPartiallyContain.test.js
@@ -13,17 +13,9 @@ describe('.toPartiallyContain', () => {
     expect(() => expect([{ a: 1, b: 2 }]).toPartiallyContain(item)).toThrowErrorMatchingSnapshot();
   });
 
-  test('fails when an array does not contain any of the properties of a given object', () => {
-    expect(() => expect([{ foo: 'bar' }]).toPartiallyContain(item)).toThrowErrorMatchingSnapshot();
-  });
-
   describe('.not.toPartiallyContain', () => {
     test('passes when an array does not contain any of the properties of a given object', () => {
       expect([{ a: 1, b: 2 }]).not.toPartiallyContain(item);
-    });
-
-    test('passes when an array does not contain any of the properties of a given object', () => {
-      expect([{ foo: 'bar' }]).not.toPartiallyContain(item);
     });
 
     test('fails when an array contains all of the properties of a given object', () => {
