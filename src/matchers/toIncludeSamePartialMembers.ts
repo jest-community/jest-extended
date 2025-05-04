2483bcc349ae6945ef87748fca7c72f580a8cf2d
Nick Papirniy <nikolaspapirniywork@gmail.com>
2025-05-04 06:35:27 +0400

    feat: add a '.toIncludeSamePartialMembers()' custom matcher (#697)
    * feat: add a '.toIncludeSamePartialMembers()' custom matcher
    
    ---------
    
    Co-authored-by: Keegan Witt <keeganwitt@gmail.com>

---
+++ src/matchers/toIncludeSamePartialMembers.ts
@@ -0,0 +1,51 @@
+import { containsEntry } from '../utils';
+
+export function toIncludeSamePartialMembers<E = unknown>(actual: unknown, expected: E) {
+  // @ts-expect-error OK to have implicit any for this
+  const { printReceived, printExpected, matcherHint } = this.utils;
+
+  // @ts-expect-error OK to have implicit any for this
+  const pass = predicate(this.equals, actual, expected);
+
+  return {
+    pass,
+    message: () =>
+      pass
+        ? matcherHint('.not.toIncludeSamePartialMembers') +
+          '\n\n' +
+          'Expected list to not exactly match the partial members of:\n' +
+          `  ${printExpected(expected)}\n` +
+          'Received:\n' +
+          `  ${printReceived(actual)}`
+        : matcherHint('.toIncludeSamePartialMembers') +
+          '\n\n' +
+          'Expected list to have the following partial members and no more:\n' +
+          `  ${printExpected(expected)}\n` +
+          'Received:\n' +
+          `  ${printReceived(actual)}`,
+  };
+}
+
+const predicate = (equals: any, actual: unknown, expected: any) => {
+  if (!Array.isArray(actual) || !Array.isArray(expected) || actual.length !== expected.length) {
+    return false;
+  }
+
+  const remaining = expected.reduce((remaining, expectedPartial) => {
+    if (remaining === null) {
+      return remaining;
+    }
+
+    const index = remaining.findIndex((actualValue: any) =>
+      Object.entries(expectedPartial).every(entry => containsEntry(equals, actualValue, entry)),
+    );
+
+    if (index === -1) {
+      return null;
+    }
+
+    return remaining.slice(0, index).concat(remaining.slice(index + 1));
+  }, actual);
+
+  return !!remaining && remaining.length === 0;
+};
